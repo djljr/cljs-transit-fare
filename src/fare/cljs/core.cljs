2af4edca188a161cb16f08919c743374354daf32
Den Lipovsky <dennis.lipovsky@gmail.com>
2014-01-06 23:31:21 -0500

    some non-trivial unit tests

--- src/fare/cljs/core.cljs
+++ src/fare/cljs/core.cljs
@@ -6,7 +6,5 @@
             [cljs.core.async :refer [put! chan <!]]))
 
 (defn is-weekday? [d]
-  not (contains? #{5 6} (.getIsoWeekday d)))
+  (not (contains? #{5 6} (.getIsoWeekday d))))
 
-(.log js/console (is-weekday? (date/Date. 2014 0 6)))
-(.log js/console (date/Date. 2014 0 6))
--- test/fare/cljs/test.cljs
+++ test/fare/cljs/test.cljs
@@ -1,7 +1,19 @@
 (ns djljr.fare.core.test
-  (:require-macros [cemerick.cljs.test :refer (deftest is)])
+  (:require-macros [cemerick.cljs.test :refer (deftest are is testing)])
   (:require [cemerick.cljs.test :as t]
+            [goog.date :as date]
             [djljr.fare.core :refer [is-weekday?]]))
+
+(deftest is-weekday-test
+  (testing "djljr.fare.core.is-weekday?"
+    (testing "well known weekdays"
+      (are [expected actual] (= expected actual)
+           true (is-weekday? (date/Date. 2014 0 6))
+           true (is-weekday? (date/Date. 2014 0 3))))
+    (testing "some weekends"
+      (are [expected actual] (= expected actual)
+           false (is-weekday? (date/Date. 2014 0 4))
+           false (is-weekday? (date/Date. 2014 0 5))))))
 
 (deftest trivial
 	(is (= 1 1)))
