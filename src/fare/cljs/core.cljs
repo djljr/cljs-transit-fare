f22ec69833fdae993e24426e65563cb0ddcf68a2
Den Lipovsky <dennis.lipovsky@gmail.com>
2014-01-11 15:56:02 -0500

    adding holidays; initialize correctly

--- src/fare/cljs/core.cljs
+++ src/fare/cljs/core.cljs
@@ -24,13 +24,15 @@
 (def state (atom {:forty-trip-price 76.00 
                   :thirty-day-price 80.00 
                   :start-date (now-string) 
-                  :holidays #{"2014-01-01" "2014-01-20" "2014-05-26" "2014-07-04" "2014-09-01" "2014-11-27" "2014-11-28" "2014-12-24" "2014-12-25" "2014-12-26"} 
+                  :holiday #{"2014-01-01" "2014-01-20" "2014-05-26" "2014-07-04" "2014-09-01" "2014-11-27" "2014-11-28" "2014-12-24" "2014-12-25" "2014-12-26"} 
                   :vacation #{} 
                   :travel-days #{}}))
 
 (defn load []
   (let [state-string (.get goog.net.cookies "state")]
-    (reset! state (reader/read-string state-string))))
+  	(if (undefined? state-string) 
+     (reset! state @state)
+     (reset! state (reader/read-string state-string)))))
 
 (defn save [st]
   (let [state-string (pr-str st)]
@@ -40,7 +42,7 @@
   (not (contains? #{5 6} (.getIsoWeekday d))))
 
 (defn is-not-holiday? [d]
-  (not (contains? (:holidays @state) (date-string d))))
+  (not (contains? (:holiday @state) (date-string d))))
 
 (defn is-not-vacation? [d]
   (not (contains? (:vacation @state) (date-string d))))
@@ -72,33 +74,15 @@
         end-date (date-obj end)
         before (fn [d] (< (date/Date.compare d end-date) 0))
         days-bet (take-while before (days start-date))]
-    (.log js/console (date-string (last days-bet)))
     (count days-bet)))
+
+(declare update)
 
 (defn listen [el type]
   (let [out (chan)]
     (events/listen el type
                    (fn [e] (put! out e)))
     out))
-
-(defn display-set [s]
-  (let [fmt (fn [value] (str "<li>" value "</li>"))]
-    (str "<ul>" (join (map fmt s)) "</ul>")))
-
-(defn update []
-  (let [current @state
-        last-day-forty-trip (date-string (last (take 20 (trip-days (days (date-obj (:start-date current)))))))]
-  	(set! (.-innerHTML (dom/getElement "thirty-day-result")) (.toFixed (/ (:thirty-day-price current) 30) 2))
-  	(set! (.-innerHTML (dom/getElement "forty-trip-result")) (.toFixed (/ (:forty-trip-price current) (days-between (:start-date current) last-day-forty-trip)) 2))
-    (set! (.-innerHTML (dom/getElement "vacation-days")) (display-set (get current :vacation)))
-    (save current)))
-
-(defn init []
-  (let [current @state]
-    (set! (.-value (dom/getElement "start-date")) (:start-date current))
-    (set! (.-value (dom/getElement "thirty-day-price")) (:thirty-day-price current))
-    (set! (.-value (dom/getElement "forty-trip-price")) (:forty-trip-price current))
-    (update)))
 
 (defn listen-assoc [el key]
   (let [text (listen el "change")]
@@ -111,6 +95,9 @@
 (defn append [st key value]
   (assoc st key (conj (get st key) value)))
 
+(defn disjunct [st key value]
+  (assoc st key (disj (get st key) value)))
+
 (defn listen-append [btn el key]
   (let [add-btn (listen btn "click")]
     (go
@@ -119,9 +106,48 @@
           (swap! state append key (-> el .-value))
           (update))))))
 
+(defn set-remove [li key]
+  (let [chan (listen li "click")]
+    (go
+      (while true
+      	(let [event (<! chan)]
+         (swap! state disjunct key (-> li .-innerHTML))
+         (update))))))
+
+(defn make-li [value]
+  (let [item (dom/createDom "span" (clj->js {"class" "label label-default"}))]
+    (set! (.-innerHTML item) value)
+    item))
+
+(defn display-set [key s display]
+  (let [lis (into-array (map make-li (sort s)))]
+  	(doseq [li lis]
+  		(set-remove li key))
+  	(dom/removeChildren display)
+  	(dom/replaceNode (dom/createDom "div" (clj->js {"class" "input-group col-sm-7" "id" (str key "-days")}) lis) display)))
+
+(defn update []
+  (let [current @state
+        last-day-forty-trip (date-string (last (take 20 (trip-days (days (date-obj (:start-date current)))))))]
+  	(set! (.-innerHTML (dom/getElement "thirty-day-result")) (.toFixed (/ (:thirty-day-price current) 30) 2))
+  	(set! (.-innerHTML (dom/getElement "forty-trip-result")) (.toFixed (/ (:forty-trip-price current) (days-between (:start-date current) last-day-forty-trip)) 2))
+    (display-set :vacation (get current :vacation) (dom/getElement ":vacation-days"))
+    (display-set :holiday (get current :holiday) (dom/getElement ":holiday-days"))
+    (save current)))
+
+(defn init []
+  (let [current @state]
+    (set! (.-value (dom/getElement "start-date")) (:start-date current))
+    (set! (.-value (dom/getElement "thirty-day-price")) (:thirty-day-price current))
+    (set! (.-value (dom/getElement "forty-trip-price")) (:forty-trip-price current))
+    (update)))
+
+
+
 (listen-assoc (dom/getElement "start-date") :start-date)
 (listen-assoc (dom/getElement "thirty-day-price") :thirty-day-price)
 (listen-assoc (dom/getElement "forty-trip-price") :forty-trip-price)
 (listen-append (dom/getElement "add-vacation-day") (dom/getElement "vacation-day") :vacation)
+(listen-append (dom/getElement "add-holiday-day") (dom/getElement "holiday-day") :holiday)
 (load)
 (init)
