9e7cc690e3072354046c1d75e45a17d7e4ece293
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2015-12-15 05:19:00 -0500

    for some reason :simple or :advanced optimizations causes the cljs compiler to error

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [reagent "0.5.1"]
                  [re-frame "0.6.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
+  :plugins [[lein-cljsbuild "1.1.2"]
             [lein-figwheel "0.5.0-2"]]
 
   :source-paths ["src"]
@@ -18,22 +18,25 @@
   :profiles {:dev
              {:dependencies [[com.cemerick/piggieback "0.2.1"]
                              [figwheel-sidecar "0.5.0-2"]]}
-             :repl {:plugins [[cider/cider-nrepl "0.9.1"]]}}
+             :repl {:plugins [[cider/cider-nrepl "0.9.1"]
+                              [refactor-nrepl "1.1.0"]]}}
   
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :cljsbuild { 
-    :builds [{:id "dev"
-              :source-paths ["src"]
-              :figwheel {:on-jsload "fare.core/mount-root"}
-              :compiler {:main fare.core
-                         :asset-path "js/compiled/out"
-                         :output-to "resources/public/js/compiled/fare.js"
-                         :outpur-dir "resources/public/js/compiled/out"
-                         :source-map-timestamp true}}
-             {:id "min"
-              :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/fare.js"
-                         :main fare.core
-                         :optimizations :advanced
-                         :pretty-print false}}]}
+              :builds {:dev
+                       {:source-paths ["src"]
+                        :figwheel {:on-jsload "fare.core/mount-root"}
+                        :compiler {:main fare.core
+                                   :asset-path "js/compiled/out"
+                                   :output-to "resources/public/js/compiled/fare.js"
+                                   :outpur-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true}}
+                       :min
+                       {:source-paths ["src"]
+                        :compiler {:main fare.core
+                                   :output-to "resources/public/js/compiled/fare.js"
+                                   :optimizations :whitespace
+                                   :pretty-print false}}}}
+  
   :figwheel {:css-dirs ["resources/public/css"]})
