2dde106ed65c35a42d3f76130fe143dfb92d3595
Dennis Lipovsky <dennis@yodle.com>
2015-12-21 10:48:49 -0500

    fixing cljsbuild - the source paths were not matching the directory structure

--- project.clj
+++ project.clj
@@ -12,8 +12,8 @@
             [lein-figwheel "0.5.0-2"]]
 
   :source-paths ["src"]
+
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-  :test-paths ["test"]
 
   :profiles {:dev
              {:dependencies [[com.cemerick/piggieback "0.2.1"]
@@ -23,20 +23,20 @@
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-  :cljsbuild {
-              :builds {:dev
-                       {:source-paths ["src"]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
                         :figwheel {:on-jsload "fare.core/mount-root"}
                         :compiler {:main fare.core
                                    :asset-path "js/compiled/out"
                                    :output-to "resources/public/js/compiled/fare.js"
                                    :outpur-dir "resources/public/js/compiled/out"
                                    :source-map-timestamp true}}
-                       :min
-                       {:source-paths ["src"]
+
+                       {:id "min"
+                        :source-paths ["src"]
                         :compiler {:main fare.core
                                    :output-to "resources/public/js/compiled/fare.js"
                                    :optimizations :advanced
-                                   :pretty-print false}}}}
+                                   :pretty-print false}}]}
 
   :figwheel {:css-dirs ["resources/public/css"]})
